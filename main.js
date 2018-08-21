baa8627ca14e4915c907d76aa78303e04da06fc6
Rosa <rosathethompson@gmail.com>
2018-08-21 18:01:52 +1000

    whoa

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 console.log('yes its connected')
 
-$('readmore').click(function() {
+$('.readmore a').click(function() {
   event.preventDefault()
-  $('p').slidedown()
-  $('readmore').text('showless')
+  $('#show-this-on-click').slideDown()
+  $('.readmore a').hide()
+  $('.readless').show()
+})
+
+$('.readless a').click(function() {
+  event.preventDefault()
+  $('#show-this-on-click').slideUp()
+  $('.readless').hide()
+  $('.readmore a').show()
+})
